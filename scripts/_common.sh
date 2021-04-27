08cdf442f6c9cb74c06d9c7a49139dcbc34cfd6b
Le Libre Au Quotidien <contact@lelibreauquotidien.fr>
2021-04-27 19:45:26 +0200

    Add remove script, add elixir/erlang dependencies

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 # dependencies used by the app
 pkg_dependencies="build-essential inotify-tools postgresql postgresql-client postgresql-contrib postgis postgresql-postgis-scripts git curl unzip gnupg openssl make gcc libc-dev argon2 imagemagick webp gifsicle jpegoptim optipng pngquant cmake"
-
+extra_pkg_dependencies="elixir esl-erlang"
 #=================================================
 # PERSONAL HELPERS
 #=================================================
